a2bb2fac2bb0619d558e29bed911e7a037b3e785
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-21 21:14:59 +0300

    lesson 1. LIMIT для ограниченного вывода строк (в примере выбираем 2 строки из столбца)

--- 1.php
+++ 1.php
@@ -9,7 +9,7 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT DISTINCT `id` FROM `plane`;";
+$sql = "SELECT `id` FROM `plane` LIMIT 2;";
 $res = mysqli_query($connect , $sql);
 
 while ($row = $res->fetch_assoc()) {
