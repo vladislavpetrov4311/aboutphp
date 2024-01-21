8d66e12a85002813f670f4d6898d2588dc3d0eae
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-21 21:29:36 +0300

    lesson 1. вывод конкретных строк

--- 1.php
+++ 1.php
@@ -9,7 +9,8 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `id` FROM `plane` LIMIT 3 OFFSET 1;";
+$sql = "SELECT `id` FROM `plane` LIMIT 1,3;";
+//краткая форма записи, выводится строки с 1 по 3
 $res = mysqli_query($connect , $sql);
 
 while ($row = $res->fetch_assoc()) {
