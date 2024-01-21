1be3bd4434fc4a85363cc9cc8b317507d9401db1
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-21 20:59:12 +0300

    lesson 1. выборка нескольких столбцов из таблицы

--- 1.php
+++ 1.php
@@ -9,12 +9,17 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `id` FROM `plane`;";
+$sql = "SELECT `id`, `vpp`, `dlina` FROM `plane`;";
 $res = mysqli_query($connect , $sql);
 
 while ($row = $res->fetch_assoc()) {
     echo $row['id']."<br>";
+    echo $row['vpp']."<br>";
+    echo $row['dlina']."<br>";
+    echo "<br><br><br>";
+
 }
+
 
 
 ?>
