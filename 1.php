32afc6cd7e6fecbe9b7188d91e3470a81d68f79b
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-21 20:35:52 +0300

    connect + вывод столбца

---
+++ 1.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = 'my_bd';
+$username = 'root';
+$password = 'root';
+$database = 'testDB';
+
+$connect = mysqli_connect($host, $username , $password, $database);
+
+
+
+$sql = "SELECT * FROM `plane`;";
+$res = mysqli_query($connect , $sql);
+
+while ($row = $res->fetch_assoc()) {
+    echo $row['id']."<br>";
+}
+
+
+?>
+
+
