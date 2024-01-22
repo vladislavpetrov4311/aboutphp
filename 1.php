f17420849b424ffca84a3f943ab7dd43e526a897
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-22 17:29:20 +0300

    lesson 1. ORDER BY (сортируем столбец)

--- 1.php
+++ 1.php
@@ -9,8 +9,8 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `id` FROM `plane` LIMIT 1,3;";
-//краткая форма записи, выводится строки с 1 по 3
+$sql = "SELECT `id` FROM `plane` ORDER BY `id`;";
+//сортируем по столбцу
 $res = mysqli_query($connect , $sql);
 
 while ($row = $res->fetch_assoc()) {
