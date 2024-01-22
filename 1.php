88a2169e2763b4c3068ceb2a8c687e46fa738db8
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-22 18:04:56 +0300

    lesson 2. вывод столбцов по нескольким отсортированным по убыванию

--- 1.php
+++ 1.php
@@ -9,7 +9,7 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `id` , `vpp` , `dlina` FROM `plane` ORDER BY `id` DESC, `dlina`;";
+$sql = "SELECT `id` , `vpp` , `dlina` FROM `plane` ORDER BY `id` DESC, `vpp` DESC;";
 
 $res = mysqli_query($connect , $sql);
 
