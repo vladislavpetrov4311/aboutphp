70e327aafc8eb1b983bed323e537561a48923ce3
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-24 17:38:20 +0300

    lesson 5. запрос на выборку, где в столбце поле начинается на букву р, а заканчивается !"

--- 1.php
+++ 1.php
@@ -9,13 +9,15 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `id` , `vpp`  FROM `plane` WHERE `vpp` LIKE 'русский%';";
+$sql = "SELECT *  FROM `plane` WHERE `vpp` LIKE 'р%!';";
 
 $res = mysqli_query($connect , $sql);
 
 while ($row = $res->fetch_assoc()) {
     echo $row['id']."<br>";
     echo $row['vpp']."<br>";
+    echo $row['dlina']."<br>";
+    echo $row['itog']."<br>";
     echo "<br>";
 
 }
