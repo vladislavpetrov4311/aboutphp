f8ad51d455c7bd7b1a60f45a316b9f36db2502c6
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-24 17:29:22 +0300

    lesson 5. оператор LIKE + один метасимвол %(начинается поле с слова 'русский')"

--- 1.php
+++ 1.php
@@ -9,14 +9,13 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `id` , `vpp` , `itog` FROM `plane` WHERE `vpp` LIKE '%текст%';";
+$sql = "SELECT `id` , `vpp`  FROM `plane` WHERE `vpp` LIKE 'русский%';";
 
 $res = mysqli_query($connect , $sql);
 
 while ($row = $res->fetch_assoc()) {
     echo $row['id']."<br>";
     echo $row['vpp']."<br>";
-    echo $row['itog']."<br>";
     echo "<br>";
 
 }
