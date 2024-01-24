e16e6bd2ca7ada252ed08a7b9b5bb24f6610e089
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-24 13:04:27 +0300

    lesson 4. использование скобок при сложной фильтрации

--- 1.php
+++ 1.php
@@ -9,12 +9,13 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `id` , `itog` FROM `plane` WHERE `id` = 30 OR `vpp` IS NULL;";
+$sql = "SELECT `id` , `vpp` , `itog` FROM `plane` WHERE (`id` = 30 OR `vpp` IS NULL) AND `itog` BETWEEN 1 AND 100;";
 
 $res = mysqli_query($connect , $sql);
 
 while ($row = $res->fetch_assoc()) {
     echo $row['id']."<br>";
+    echo $row['vpp']."<br>";
     echo $row['itog']."<br>";
     echo "<br>";
 
