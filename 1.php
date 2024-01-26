b54c57794c22acff3bf8489a24cb01f3c67e0bb5
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-26 18:21:07 +0300

    lesson 7. функция YEAR для возврата строк, соотв. конретному году

--- 1.php
+++ 1.php
@@ -9,7 +9,7 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `id`, `vpp`  FROM `plane` WHERE SOUNDEX(`vpp`) LIKE SOUNDEX('петров');";
+$sql = "SELECT `id`, `vpp`  FROM `plane` WHERE YEAR(`data`) = '2024';";
 
 $res = mysqli_query($connect , $sql);
 
