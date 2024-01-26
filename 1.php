6c4914925b63bce95db7b35d8f50e14fded8e3d8
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-26 23:33:47 +0300

    lesson 8. AVG для нескольких столбцов

--- 1.php
+++ 1.php
@@ -9,12 +9,13 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT AVG(`itog`) AS `result` FROM `plane`;";
+$sql = "SELECT AVG(`itog`) AS `result`, AVG(`id`) AS `result2` FROM `plane`;";
 
 $res = mysqli_query($connect , $sql);
 
 while ($row = $res->fetch_assoc()) {
     echo $row['result']."<br>";
+    echo $row['result2']."<br>";
     echo "<br>";
 
 }
