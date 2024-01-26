2a5b685eee76240589bbcc1c7521ea2e29fef1c5
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-26 23:54:34 +0300

    lesson 8. вычисления над несколькими столбцами

--- 1.php
+++ 1.php
@@ -9,7 +9,7 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT SUM(`itog`) AS `result` FROM `plane` WHERE `id` = 30;";
+$sql = "SELECT SUM(`itog` * `id`) AS `result` FROM `plane` WHERE `id` = 30;";
 
 $res = mysqli_query($connect , $sql);
 
