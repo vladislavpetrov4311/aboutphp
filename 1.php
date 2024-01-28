24aed5333882b5559880b8d48514595502211180
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-28 18:15:07 +0300

    lesson 10. многоуровневый подзапрос

--- 1.php
+++ 1.php
@@ -9,12 +9,18 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `vpp` FROM `plane` WHERE `id` IN (SELECT `test` FROM `mytest`);";
+$sql = "SELECT `vpp` , `dlina` FROM `plane` WHERE `id` IN (SELECT `test`   
+                                                           FROM `mytest`
+                                                           WHERE `test` IN(
+                                                            SELECT `test2`
+                                                            FROM `mytest2`)
+                                                           );";
 
 $res = mysqli_query($connect , $sql);
 
 while ($row = $res->fetch_assoc()) {
     echo $row['vpp']."<br>";
+    echo $row['dlina']."<br>";
     echo "<br>";
 
 }
