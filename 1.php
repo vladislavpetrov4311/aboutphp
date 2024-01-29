1904a85cd84b2ce3085822b1578f2fec75b574a1
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-29 20:27:32 +0300

    lesson 11. объединение таблиц

--- 1.php
+++ 1.php
@@ -9,14 +9,14 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `test` , (SELECT COUNT(`test`) FROM `mytest` WHERE `mytest`.`test` = `mytest2`.`test`) AS `result` FROM `mytest2`;";
+$sql = "SELECT `vpp` , `dlina` , `test2` FROM `plane` , `mytest` WHERE (`mytest`.`test2` = `plane`.`id`) AND NOT `vpp` IS NULL;";
 
 $res = mysqli_query($connect , $sql);
 
 
 while ($row = $res->fetch_assoc()) {
-    echo $row['test']."<br>";
-    echo $row['result']."<br>";
+    echo $row['vpp']."<br>";
+    echo $row['dlina']."<br>";
     echo "<br>";
 
 }
