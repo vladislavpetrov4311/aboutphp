f7c3d031b0900b1616186ec51ba0895598dff493
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-30 21:05:07 +0300

    lesson 12. левое внешнеее объединение

--- 1.php
+++ 1.php
@@ -9,13 +9,14 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `P`.`vpp`  FROM `plane` AS `P` INNER JOIN `plane` AS `P2` ON (`P`.`id` = `P2`.`id`) AND NOT `P`.`vpp` IS NULL;";
+$sql = "SELECT `id` , `test2` FROM `plane` LEFT OUTER JOIN `mytest`  ON `plane`.`id` = `mytest`.`test2`;";
 
 $res = mysqli_query($connect , $sql);
 
 
 while ($row = $res->fetch_assoc()) {
-    echo $row['vpp']."<br>";
+    echo $row['id']."<br>";
+    echo $row['test2']."<br>";
     echo "<br>";
 
 }
