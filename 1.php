363a865ede70c712d31558f6cf0aad41e7157e5f
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-01-30 21:54:49 +0300

    lesson 12. итоговые функции для объединений

--- 1.php
+++ 1.php
@@ -9,14 +9,13 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 // FULL JOIN в mysql не поддерживается, но обойдём это, путём операции UNION 
-$sql = "SELECT `id` , `test2` FROM `plane` LEFT OUTER JOIN `mytest`  ON `plane`.`id` = `mytest`.`test2` UNION 
-        SELECT `id` , `test2` FROM `plane` RIGHT OUTER JOIN `mytest`  ON `plane`.`id` = `mytest`.`test2`;";
+$sql = "SELECT COUNT(`id`) AS `res`, `test2` FROM `plane` INNER JOIN `mytest`  ON `plane`.`id` = `mytest`.`test2` GROUP BY `id`;";
 
 $res = mysqli_query($connect , $sql);
 
 
 while ($row = $res->fetch_assoc()) {
-    echo $row['id']."<br>";
+    echo $row['res']."<br>";
     echo $row['test2']."<br>";
     echo "<br>";
 
