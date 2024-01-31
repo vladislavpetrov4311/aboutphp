690d58a0ac46f6c5cf6a9a471133db8cc1d97d0f
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-01 01:47:47 +0300

    lesson 13. UNION ALL, чтоб вывести даже повторяющиеся результаты строк

--- 1.php
+++ 1.php
@@ -8,10 +8,9 @@ $database = 'testDB';
 $connect = mysqli_connect($host, $username , $password, $database);
 
 
-
 $sql = "SELECT `id`, `new_test`FROM `plane`, `new` WHERE `id` = `new_test` 
-        UNION 
-        SELECT `new_test`, `id` FROM `new`, `plane`;";
+        UNION ALL
+        SELECT `id`, `new_test` FROM `new`, `plane` WHERE `new_test` = 76 AND `id` = `new_test`;";
 
 $res = mysqli_query($connect , $sql);
 
