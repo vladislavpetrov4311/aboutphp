befb328b3b77d052e63efd62fe6126bf8e36759c
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-01 01:02:50 +0300

    lesson 12. модифицированное объединение (всё, что не plane)

--- 1.php
+++ 1.php
@@ -9,15 +9,14 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `new`.`new_id` , `id` , `test2` FROM `plane` INNER JOIN `mytest` ON (`id` = `test2`) RIGHT JOIN `new` ON (`id` = `new`.`new_id`)";
+$sql = "SELECT `id` , `new_test` FROM `plane` RIGHT JOIN `new` ON (`new_test` = `id`) WHERE `id` IS NULL;";
 
 $res = mysqli_query($connect , $sql);
 
 
 while ($row = $res->fetch_assoc()) {
-    echo $row['new_id']."<br>";
     echo $row['id']."<br>";
-    echo $row['test2']."<br>";
+    echo $row['new_test']."<br>";
     echo "<br>";
 
 }
