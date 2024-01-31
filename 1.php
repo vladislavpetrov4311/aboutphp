773c03eb20e4e653e366e06ae45d1e7f692e6b8d
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-01 01:28:55 +0300

    lesson 13. комбинирование запросов

--- 1.php
+++ 1.php
@@ -9,7 +9,9 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 
 
-$sql = "SELECT `id` , `new_test` FROM `plane` RIGHT JOIN `new` ON (`new_test` = `id`) WHERE `id` IS NULL;";
+$sql = "SELECT `id` FROM `plane` WHERE `id` BETWEEN 25 AND 76
+        UNION 
+        SELECT `new_test` FROM `new`;";
 
 $res = mysqli_query($connect , $sql);
 
