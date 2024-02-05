9ff64b1b41cbdd8d51b50fc5700d14634d50e60f
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-05 04:17:27 +0300

    lesson 17. использование представлений

--- 1.php
+++ 1.php
@@ -7,21 +7,19 @@ $database = 'testDB';
 
 $connect = mysqli_connect($host, $username , $password, $database);
 
-//представление - таблица, содержащая sql запрос
-$sql = "CREATE VIEW `my_pred` AS 
-                                SELECT `id` , `test1` , `test2` , `new2` FROM `new` INNER JOIN `plane` 
-                                ON (`id` = `new2`) INNER JOIN `mytest` ON (`new2` = `test1` OR `new2`= `test2`);";
+
+$sql = "SELECT `id` , `new2` FROM `my_pred` WHERE `id` = 76;";
 
 $res = mysqli_query($connect , $sql);
 
-/*
+
 while ($row = $res->fetch_assoc()) {
     echo $row['id']."<br>";
-    echo $row['new_test']."<br>";
+    echo $row['new2']."<br>";
     echo "<br>";
 
 }
-*/
+
 
 
 ?>
