d10a3b72264da0a1559834709fe518ddf29067db
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-05 04:15:08 +0300

    lesson 17. представления

--- 1.php
+++ 1.php
@@ -7,8 +7,10 @@ $database = 'testDB';
 
 $connect = mysqli_connect($host, $username , $password, $database);
 
-
-$sql = "RENAME TABLE `new5` TO `new6`;";
+//представление - таблица, содержащая sql запрос
+$sql = "CREATE VIEW `my_pred` AS 
+                                SELECT `id` , `test1` , `test2` , `new2` FROM `new` INNER JOIN `plane` 
+                                ON (`id` = `new2`) INNER JOIN `mytest` ON (`new2` = `test1` OR `new2`= `test2`);";
 
 $res = mysqli_query($connect , $sql);
 
