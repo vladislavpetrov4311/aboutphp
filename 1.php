08a491f119313ac69a80c4702d2b20c92b30ca2b
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-05 04:45:36 +0300

    lesson 17. DROP VIEW

--- 1.php
+++ 1.php
@@ -8,17 +8,17 @@ $database = 'testDB';
 $connect = mysqli_connect($host, $username , $password, $database);
 
 
-$sql = "SELECT `res` FROM `my_pred` WHERE NOT `res` IS NULL;";
+$sql = "DROP VIEW `my_pred`;";
 
 $res = mysqli_query($connect , $sql);
 
-
+/*
 while ($row = $res->fetch_assoc()) {
     echo $row['res']."<br>";
     echo "<br>";
 
 }
-
+*/
 
 
 ?>
