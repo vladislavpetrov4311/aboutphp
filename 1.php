893cf1c554df6ccc6c89fee87ee0caf679c474dc
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-07 09:18:59 +0300

    lesson 19. предупреждение

--- 1.php
+++ 1.php
@@ -7,45 +7,30 @@ $database = 'testDB';
 
 $connect = mysqli_connect($host, $username , $password, $database);
 
-
+//ПРЕДУПРЕЖДЕНИЕ !!! 
+//транзакция с явной ошибкой на стороне клиента всё равно выполняется
+//нужно использовать транзакции только на стороне сервера СУБД 
 $sql = "START TRANSACTION;";
 $res = mysqli_query($connect , $sql);
 
-$sql1 = "SAVEPOINT `A`;";
-$res1 = mysqli_query($connect , $sql1);
-
-$sql2 = "INSERT INTO `new` (`new_test`) VALUES (2201);";
+$sql2 = "INSERT INTO `new` VALUES (9090);";
 $res2 = mysqli_query($connect , $sql2);
 
-$sql_ = "SAVEPOINT `B`;";
-$res_ = mysqli_query($connect , $sql_);
-
-$sql3 = "SELECT `new_test` FROM `new`;";
+$sql3 = "INSERT INTO `new` VALUES ('qwert');";
 $res3 = mysqli_query($connect , $sql3);
 
+$sql4 = "COMMIT;";
+$res = mysqli_query($connect , $sql4);
 
-while ($row = $res3->fetch_assoc()) {
-    echo $row['new_test']."<br>";
-    echo "<br>";
-
-}
-
-
-$sql4 = "ROLLBACK TO `B`;";
-$res4 = mysqli_query($connect , $sql4);
 
 $sql5 = "SELECT `new_test` FROM `new`;";
 $res5 = mysqli_query($connect , $sql5);
 
-
 while ($row = $res5->fetch_assoc()) {
     echo $row['new_test']."<br>";
     echo "<br>";
+
 }
-
-
-$sql6 = "COMMIT;";
-$res6 = mysqli_query($connect , $sql6);
 
 
 ?>
