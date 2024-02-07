fde3f44449be67f83c45f1d26f8aee31f51499fe
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-07 08:44:26 +0300

    lesson 19. SAVEPOINT и ROLLBACK TO

--- 1.php
+++ 1.php
@@ -10,18 +10,37 @@ $connect = mysqli_connect($host, $username , $password, $database);
 
 $sql = "START TRANSACTION;";
 $res = mysqli_query($connect , $sql);
-$sql1 = "INSERT INTO `new` (`new_test`) VALUES (200);";
+
+$sql1 = "SAVEPOINT `A`;";
 $res1 = mysqli_query($connect , $sql1);
-$sql2 = "COMMIT;";
+
+$sql2 = "INSERT INTO `new` (`new_test`) VALUES (901);";
 $res2 = mysqli_query($connect , $sql2);
 
-/*
-while ($row = $res->fetch_assoc()) {
-    echo $row['dlina']."<br>";
+$sql3 = "SELECT `new_test` FROM `new`;";
+$res3 = mysqli_query($connect , $sql3);
+
+
+while ($row = $res3->fetch_assoc()) {
+    echo $row['new_test']."<br>";
     echo "<br>";
 
 }
-*/
+
+
+$sql4 = "ROLLBACK TO `A`;";
+$res4 = mysqli_query($connect , $sql4);
+
+$sql5 = "SELECT `new_test` FROM `new`;";
+$res5 = mysqli_query($connect , $sql5);
+
+
+while ($row = $res5->fetch_assoc()) {
+    echo $row['new_test']."<br>";
+    echo "<br>";
+
+}
+
 
 
 ?>
