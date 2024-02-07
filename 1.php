42214347d4567ecd725ac1e68fe0434e83308245
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-07 08:48:24 +0300

    lesson 19. несколько SAVEPOINT и в завершение COMMIT

--- 1.php
+++ 1.php
@@ -14,8 +14,11 @@ $res = mysqli_query($connect , $sql);
 $sql1 = "SAVEPOINT `A`;";
 $res1 = mysqli_query($connect , $sql1);
 
-$sql2 = "INSERT INTO `new` (`new_test`) VALUES (901);";
+$sql2 = "INSERT INTO `new` (`new_test`) VALUES (2201);";
 $res2 = mysqli_query($connect , $sql2);
+
+$sql_ = "SAVEPOINT `B`;";
+$res_ = mysqli_query($connect , $sql_);
 
 $sql3 = "SELECT `new_test` FROM `new`;";
 $res3 = mysqli_query($connect , $sql3);
@@ -28,7 +31,7 @@ while ($row = $res3->fetch_assoc()) {
 }
 
 
-$sql4 = "ROLLBACK TO `A`;";
+$sql4 = "ROLLBACK TO `B`;";
 $res4 = mysqli_query($connect , $sql4);
 
 $sql5 = "SELECT `new_test` FROM `new`;";
@@ -38,9 +41,11 @@ $res5 = mysqli_query($connect , $sql5);
 while ($row = $res5->fetch_assoc()) {
     echo $row['new_test']."<br>";
     echo "<br>";
-
 }
 
+
+$sql6 = "COMMIT;";
+$res6 = mysqli_query($connect , $sql6);
 
 
 ?>
