cac53511619f0a9ab4bd1a9a0b2bb684c29b436f
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-07 08:37:26 +0300

    lesson 19. Транзакции (создание, сохранение)

--- 1.php
+++ 1.php
@@ -8,9 +8,12 @@ $database = 'testDB';
 $connect = mysqli_connect($host, $username , $password, $database);
 
 
-$sql = "DROP PROCEDURE `my_proc`;";
-
+$sql = "START TRANSACTION;";
 $res = mysqli_query($connect , $sql);
+$sql1 = "INSERT INTO `new` (`new_test`) VALUES (200);";
+$res1 = mysqli_query($connect , $sql1);
+$sql2 = "COMMIT;";
+$res2 = mysqli_query($connect , $sql2);
 
 /*
 while ($row = $res->fetch_assoc()) {
