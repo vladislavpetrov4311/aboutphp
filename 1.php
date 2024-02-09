fe7942d195801fbc5309ba99b32e1e2c468fcd5d
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-09 12:26:56 +0300

    lesson 21. ограничения (добавление первичного ключа)

--- 1.php
+++ 1.php
@@ -7,30 +7,19 @@ $database = 'testDB';
 
 $connect = mysqli_connect($host, $username , $password, $database);
 
-//ПРЕДУПРЕЖДЕНИЕ !!! 
-//транзакция с явной ошибкой на стороне клиента всё равно выполняется
-//нужно использовать транзакции только на стороне сервера СУБД 
-$sql = "START TRANSACTION;";
+
+$sql = "ALTER TABLE `Q1` 
+        ADD CONSTRAINT PRIMARY KEY (`id2`);";
 $res = mysqli_query($connect , $sql);
 
-$sql2 = "INSERT INTO `new` VALUES (9090);";
-$res2 = mysqli_query($connect , $sql2);
 
-$sql3 = "INSERT INTO `new` VALUES ('qwert');";
-$res3 = mysqli_query($connect , $sql3);
-
-$sql4 = "COMMIT;";
-$res = mysqli_query($connect , $sql4);
-
-
-$sql5 = "SELECT `new_test` FROM `new`;";
-$res5 = mysqli_query($connect , $sql5);
-
-while ($row = $res5->fetch_assoc()) {
+/*
+while ($row = $res->fetch_assoc()) {
     echo $row['new_test']."<br>";
     echo "<br>";
 
 }
+*/
 
 
 ?>
