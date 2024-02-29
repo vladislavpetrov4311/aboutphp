760ba99d4a92fa7ca01895e15399b7ac73bae09c
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-29 14:18:57 +0300

    подготовленный запрос (связка prepare/execute)

--- 1.php
+++ 1.php
@@ -11,9 +11,11 @@ $pdo = new PDO("mysql:host=$host;dbname=$database;", $username, $password);
 
 $myid = '1;DELETE FROM `Q1`;';
 
-$sql = "SELECT * FROM `Q1` WHERE `id` = $myid;";
+$sql = "SELECT * FROM `Q1` WHERE `id` = ?;";
 
-$res = $pdo->query($sql);
+$res = $pdo->prepare($sql);
+
+$res->execute([$myid]);
 
 
 $user = $res->fetch(PDO::FETCH_ASSOC);
