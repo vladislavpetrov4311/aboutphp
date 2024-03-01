7fd05a1701a398ae1b4a65e0e566d8a817ede0e4
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-03-01 10:24:55 +0300

    :metka передаем параметры в виде ассоциативного массива

--- 1.php
+++ 1.php
@@ -9,14 +9,15 @@ $database = 'testDB';
 $pdo = new PDO("mysql:host=$host;dbname=$database;", $username, $password);
 
 
-$myid = '1;DELETE FROM `Q1`;';
-$tittle = 'титл_тест';
+$myid = '1';
 
-$sql = "SELECT * FROM `Q1` WHERE `id` = ? AND `tittle` = ?;";
+$sql = "SELECT * FROM `Q1` WHERE `id` = :user;";
 
 $res = $pdo->prepare($sql);
 
-$res->execute([$myid , $tittle]);
+$res->execute([
+    ':user' => $myid
+]);
 
 
 $user = $res->fetch(PDO::FETCH_ASSOC);
