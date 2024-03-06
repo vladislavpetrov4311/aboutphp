7c8f9f8e4d846626f2cb3c3dd8978916e12bc40b
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-03-06 18:01:26 +0300

    lesson 7. статические свойства и методы, доступ извне

--- 1.php
+++ 1.php
@@ -1,64 +1,20 @@
 <?php
 
-final class User
+class user
 {
-    protected $name;
-    protected $pass;
 
-    public function __construct($param , $password)
-    {
-        $this->name = $param;
-        $this->pass = $password;
-    }
+    public static $name;
 
-    public function getname()
+    public static function getinfo()
     {
-        $res = "{$this->name}".' '."{$this->pass}";
-        return $res;
-    }
-
-    final public function test($mytest)
-    {
-        return $this->pass = $mytest;    
+        return self::$name;
     }
 
 }
 
 
-class Admin extends User
-{
-    
-    protected $level;
-
-    public function __construct($param , $password, $Mlevel)
-    {
-        parent::__construct($param, $password);
-        $this->level = $Mlevel;
-    }
-        
-    
-
-    public function getname()
-    {
-         $main_res = parent::getname();
-         $main_res.= " {$this->level}";
-         return $main_res;
-    }
-
-    public function test($main)
-    {
-        return $main;  
-    }
-    
-}
-
-$admin = new Admin("admin" , "root" , "main");
-$inf = $admin->getname();
-echo $inf;
-
-$A = $admin->test("123");
-echo $A;
-
-
-
+$res = user::$name="admin";
+echo $res;
+$res2 = user::getinfo();
+echo $res2;
 ?>
