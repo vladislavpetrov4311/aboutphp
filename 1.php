e4254f5fe789aaf90560fbed5144ac976eea684b
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-03-08 02:34:43 +0300

    lesson 11. трейты (имитация множественного наследования)

--- 1.php
+++ 1.php
@@ -1,70 +1,35 @@
 <?php
 
-abstract class User
+trait User
 {
-    public $name;
-}
-
-abstract class User2 extends User
-{
-    abstract public function getinfo();
-    //абстрактный класс позволяет реализовывать обычные методы
-    public function test()
-    {
-        return $this->name = "123";
-    }
-}
-
-class Admin extends User2
-{
-
-    public function getinfo()
-    {
-        return $this->name = 11;
-    }
-
-}
-
-class Qwer extends User2
-{
-    //переопределяем абстрактный метод
-    public function getinfo()
-    {
-
-    }
-}
-
-
-
-class Main extends User2
-{
-
-    private $my_test;
-    public function getinfo()
-    {
-        return $this->name = "qwert";
-    }
-
+    public $name = "qwer";
     public function getname()
     {
-        return $this->name = 1+2;
+        return $this->name;
     }
-
-    public function test()
-    {
-        $this->my_test = parent::test();
-        return $this->name = $this->my_test;
-    }
-
 }
+
+trait User2
+{
+    public $status;
+    public function getstatus()
+    {
+        return $this->status;
+    }
+}
+
+class Admin
+{
+    use User, User2;
+    public function __construct()
+    {
+        $this->name = "user";
+        $this->status = "root";
+    }
+}
+
 $admin = new Admin();
-echo $admin->getinfo();
-
-$main = new Main();
-echo $main->getinfo();
-echo $main->getname();
-
-echo $main->test();
+echo $admin->getname().' '.$admin->getstatus();
 
 
 ?>
